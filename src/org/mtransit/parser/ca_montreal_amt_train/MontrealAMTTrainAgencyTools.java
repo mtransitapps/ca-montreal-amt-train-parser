6150ed1f3256d8871902fc8e58cc87d051ea635f
Mathieu Méa <mathieu.mea@gmail.com>
2015-03-16 14:22:02 -0400

    Minor

--- src/org/mtransit/parser/ca_montreal_amt_train/MontrealAMTTrainAgencyTools.java
+++ src/org/mtransit/parser/ca_montreal_amt_train/MontrealAMTTrainAgencyTools.java
@@ -91,7 +91,7 @@ public class MontrealAMTTrainAgencyTools extends DefaultAgencyTools {
 	@Override
 	public void setTripHeadsign(MRoute route, MTrip mTrip, GTrip gTrip) {
 		String stationName = cleanTripHeadsign(gTrip.trip_headsign);
-		int directionId = Integer.valueOf(gTrip.direction_id);
+		int directionId = gTrip.direction_id;
 		mTrip.setHeadsignString(stationName, directionId);
 	}
 
